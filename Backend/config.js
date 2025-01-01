794173965bb1dadd4d9229b4b9d451d6d2acb406
JABRI Raed <jabriraed80@gmail.com>
2025-01-01 15:51:57 +0100

    add all crud for repairmodel and correct old code . all crud work (test with postman )

--- Backend/config.js
+++ Backend/config.js
@@ -1,2 +1,2 @@
-export const Port=8083;
+export const Port=5555;
 export const mongoDBURL="mongodb+srv://jabriraed80:1v54STOJo3y2XHSS@cluster0.b6kff.mongodb.net/gestionparcv1?retryWrites=true&w=majority&appName=Cluster0"
--- Backend/index.js
+++ Backend/index.js
@@ -3,8 +3,25 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import { Port } from './config.js';
 import {mongoDBURL} from './config.js';
+import RequestRepair from './Routes/RequestRepair.js';
 
 const app = express();
+//middleware for parsing requests body 
+app.use(express.json());
+
+
+app.use(cors());
+
+//just for test backend 
+app.get('/',(req,res) => {
+        console.log(req);
+        return res.status(234).send("harbi weld el coba");
+})
+
+
+
+app.use('/requestrepair', RequestRepair);
+
 
 
 

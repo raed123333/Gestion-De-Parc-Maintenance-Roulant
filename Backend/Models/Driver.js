ebae78734bdc2933dc2664f54b182c448048c5be
JABRI Raed <jabriraed80@gmail.com>
2025-01-01 11:23:53 +0100

    create DriverModel

---
+++ Backend/Models/Driver.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+const DriverSchema = new mongoose.Schema({
+        FirstName:String,
+                LastName:String,
+                RegistrationNumber:String,
+                Email:String,
+                Password:String
+        
+})
+
+const DriverModel=mongoose.model("Driver",DriverSchema);
+export default DriverModel

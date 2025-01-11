b35f602045a2b0fb1578ece8992869c939285d8b
JABRI Raed <jabriraed80@gmail.com>
2025-01-11 14:29:11 +0100

    update index.js for deploy app

--- Backend/index.js
+++ Backend/index.js
@@ -14,7 +14,8 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: ["https://deploy-mern-1whq.vercel.app"],
+  methods:["POST","GET"],
   credentials: true,
 }));
 
